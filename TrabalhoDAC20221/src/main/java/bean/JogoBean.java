2535566bab12879f49b9b6bb5e4800a8f8c76157
jose_ildaci@hotmail.com <jose_ildacaci@hotmail.com>
2022-05-29 19:47:40 -0300

    APSAP1DAC20221

---
+++ TrabalhoDAC20221/src/main/java/bean/JogoBean.java
@@ -0,0 +1,84 @@
+package bean;
+
+import java.util.List;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+
+
+import dao.JogoDao;
+import entidade.Jogo;
+
+@ManagedBean
+public class JogoBean {
+	private Jogo jogo = new Jogo();
+	private List<Jogo> listaJogo;
+
+	public String salvar() {
+		try {
+			jogo.gerarNumerosAleatorios();
+			JogoDao.salvar(jogo);
+			jogo = new Jogo();
+
+			FacesContext.getCurrentInstance().addMessage(null,
+					new FacesMessage(FacesMessage.SEVERITY_INFO, null, "Salvo com successo!"));
+		} catch (Exception e) {
+			jogo = new Jogo();
+			FacesContext.getCurrentInstance().addMessage(null,
+					new FacesMessage(FacesMessage.SEVERITY_ERROR, "Falha ao salvar!", "Os dados não foram salvos!"));
+		}
+		return null;
+	}
+
+	public String editar() {
+		try {
+			JogoDao.editar(jogo);
+			FacesContext.getCurrentInstance().addMessage(null,
+					new FacesMessage(FacesMessage.SEVERITY_INFO, null, "Editado com sucesso!"));
+		} catch (Exception e) {
+			FacesContext.getCurrentInstance().addMessage(null,
+					new FacesMessage(FacesMessage.SEVERITY_ERROR, "Falha!", "Os dados não foram editados!"));
+		}
+		return null;
+	}
+
+	public String excluir() {
+		try {
+			JogoDao.excluir(jogo);
+			listaJogo.remove(jogo);
+
+			FacesContext.getCurrentInstance().addMessage(null,
+					new FacesMessage(FacesMessage.SEVERITY_INFO, null, "Excluido com sucesso!"));
+		} catch (Exception e) {
+			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR,
+					"Falha ao excluir!", "Os dados não forma excluidos!"));
+		}
+		return null;
+	}
+
+	public String listar() {
+		listaJogo = JogoDao.listar();
+		return "listagem.xhtml";
+	}
+
+	public Jogo getJogo() {
+		return jogo;
+	}
+
+	public void setJogo(Jogo jogo) {
+		this.jogo = jogo;
+	}
+
+	public List<Jogo> getListaJogo() {
+		if (listaJogo == null) {
+			listaJogo = JogoDao.listar();
+		}
+		return listaJogo;
+	}
+
+	public void setListaJogo(List<Jogo> listaJogo) {
+		this.listaJogo = listaJogo;
+	}
+
+}
---
+++ TrabalhoDAC20221/src/main/java/dao/JogoDao.java
@@ -0,0 +1,45 @@
+package dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+import entidade.Jogo;
+import util.JPAUtil;
+
+public class JogoDao {
+	public static void salvar(Jogo j) {
+		EntityManager em = JPAUtil.criarEntityManager();
+		em.getTransaction().begin();
+		em.persist(j);
+		em.getTransaction().commit();
+		em.close();
+	}
+
+	public static void editar(Jogo j) {
+		EntityManager em = JPAUtil.criarEntityManager();
+		em.getTransaction().begin();
+		em.merge(j);
+		em.getTransaction().commit();
+		em.close();
+	}
+
+	public static void excluir(Jogo j) {
+		EntityManager em = JPAUtil.criarEntityManager();
+		em.getTransaction().begin();
+		j = em.find(Jogo.class, j.getId());
+		em.remove(j);
+		em.getTransaction().commit();
+		em.close();
+	}
+
+	@SuppressWarnings("unchecked")
+	public static List<Jogo> listar() {
+		EntityManager em = JPAUtil.criarEntityManager();
+		Query query = em.createQuery("select j from Jogo j");
+		List<Jogo> lista = query.getResultList();
+		return lista;
+	}
+
+}
